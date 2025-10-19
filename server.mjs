cd3d38d334fc3c8cf4bb91867df409de9d61f399
hasanhayat <aylskhsmvz.v@gmail.com>
2025-10-19 19:55:59 +0500

    ll

--- server.mjs
+++ server.mjs
@@ -23,10 +23,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-    origin: ["http://localhost:3000",'https://healthcare-ai-by-hassan.vercel.app',"*"], // ya deployed frontend URL
-    credentials: true,
-  }));
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use(express.json());
