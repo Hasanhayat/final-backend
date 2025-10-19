283cfd33f2c57f685f8e55a68f821a8b46dad6e7
hasanhayat <aylskhsmvz.v@gmail.com>
2025-10-19 19:38:35 +0500

    ll

--- server.mjs
+++ server.mjs
@@ -16,8 +16,8 @@ import { fileURLToPath } from "url";
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
